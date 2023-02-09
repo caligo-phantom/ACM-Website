7d720379cb96508417148a3e821dbb6a1553622e
caligo-phantom <siddharthmittal2002@gmail.com>
2023-02-09 19:11:12 +0530

    updated popup

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-window.onload = function() {
-    var modal = document.getElementById("popup1");
-    var btn = document.getElementsByClassName("button")[0];
-    var span = document.getElementsByClassName("close")[0];
-
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
+$(document).ready(function () {
+    $('#trigger').click(function () {
+        $('#overlay').fadeIn(300);
+    });
+    $('#close').click(function () {
+        $('#overlay').fadeOut(300);
+    });
+});
